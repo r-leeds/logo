22bbba535c351c701fec1a6a8a4b9b86a5db4d70
Ed Berry <edward.berry@skybettingandgaming.com>
2019-10-15 10:40:43 +0100

    Tweak hex sticker to move text

--- logo.R
+++ logo.R
@@ -2,6 +2,8 @@
 library(ggplot2)
 library(viridis)
 library(hexSticker)
+# my font issues were fixed by extrafonts::font_import()
+library(extrafont) 
 
 # Palette -----------------------------------------------------------------
 # create a palette from viridis
@@ -10,66 +12,47 @@ pal = viridis(3, end = 0.8)
 
 # Plot parameters ---------------------------------------------------------
 # some parameters for the plot
-text_size = 16
-url_text_size = 3
+text_size = 4.5
+url_text_size = 1.5
 text_color = '#2F4F4F'
-# line_size = 1.75
 background_color = '#F5F5F5'
 
 
 # Sticker plot ------------------------------------------------------------
-# Create original plot
-# ggplot(NULL, aes(x = 0:5)) +
-#   # plot Weibull distributions with different parameters
-#   stat_function(fun = dweibull, args = list(shape = 1), color = pal[1], size = line_size) +
-#   stat_function(fun = dweibull, args = list(shape = 2), color = pal[2], size = line_size) +
-#   stat_function(fun = dweibull, args = list(shape = 4), color = pal[3], size = line_size) +
-#   # add the text
-#   annotate("text", x = 3.25, y = 1.25, label = "R", family = "mono", size = text_size, color = text_color) +
-#   annotate("text", x = 3.25, y = 1, label = "Users", family = "mono", size = text_size, color = text_color) +
-#   annotate("text", x = 3.25, y = 0.75, label = "Leeds", family = "mono", size = text_size, color = text_color) +
-#   # remove all the plot elements
-#   theme_void() +
-#   # change the background color and margins
-#   theme(plot.background = element_rect(fill = background_color),
-#         plot.margin = unit(c(1.5, 1.5, 1.5, 1.5), "cm")) 
-# 
-# ggsave("logo.png", width = 5, height = 5, dpi = 'retina')
-
 # Hex subplot
 hex_plot <- ggplot(NULL, aes(x = 0:5)) +
   # plot Weibull distributions with different parameters
   stat_function(fun = dweibull, args = list(shape = 1), color = pal[1], size = 1) +
   stat_function(fun = dweibull, args = list(shape = 2), color = pal[2], size = 1) +
   stat_function(fun = dweibull, args = list(shape = 4), color = pal[3], size = 1) +
-  # add the text
   # remove all the plot elements
   theme_void() + 
   theme_transparent()
 
 # Plot hex
-sticker(hex_plot, package = "R Users Leeds", 
-        # Modify plot_new par
-        s_x = 1, 
-        s_y = 0.85, 
-        s_width = 1.35, 
-        s_height = 1, 
-        # Modify package name par
-        p_size = text_size, 
-        p_color = text_color, 
-        p_y = 1.45, 
-        p_family = "Aller_Lt",
-        # Modify hexagon par
-        h_fill = background_color,
-        h_color = pal[3],
-        # Modify url par
-        url = "r-users-leeds.netlify.com",
-        u_size = url_text_size, 
-        u_color = "#2F4F4F",
-        u_family = "Aller_Lt",
-        # Output par
-        dpi = "retina", filename = "rleeds_hex_logo.png")
+hex_sticker <- sticker(hex_plot, package = "R\nUsers\nLeeds", 
+        s_x = 1, # plot x pos
+        s_y = 1.075, # plot y pos
+        s_width = 1.4, # plot width
+        s_height = 1.1, # plot hieight
+        p_size = text_size, # text size
+        p_color = text_color, # text color
+        p_x = 1.275, # text x pos
+        p_y = 1.15,  # text y pos
+        p_family = "mono", # font family
+        h_fill = background_color, # hex fill color
+        h_color = pal[3], # hex border color
+        url = "r-users-leeds.netlify.com", 
+        u_size = url_text_size, # url text size
+        u_color = "#2F4F4F", # url color
+        u_family = "mono" # url font family
+        )
 
+# save out
+save_sticker(
+  'rleeds_hex_sticker.png',
+  hex_sticker
+)
 
 # Printing ----------------------------------------------------------------
 # https://github.com/rladies/starter-kit/tree/master/stickers
